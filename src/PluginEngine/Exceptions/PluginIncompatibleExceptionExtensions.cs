2db1af5e05c32b2ef054b3778792dee34fc0cb2f
Vladyslav Zaiets <rutova2@gmail.com>
2024-06-11 09:44:42 +0300

    more PluginIncompatibleException extension methods

--- src/PluginEngine/Exceptions/PluginIncompatibleExceptionExtensions.cs
+++ src/PluginEngine/Exceptions/PluginIncompatibleExceptionExtensions.cs
@@ -2,20 +2,44 @@ using System;
 
 namespace PluginEngine.Exceptions
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="PluginIncompatibleException"/> to analyze and describe version incompatibility issues.
+    /// </summary>
     public static class PluginIncompatibleExceptionExtensions
     {
+        /// <summary>
+        /// Determines whether the plugin version is incompatible with the host engine version.
+        /// </summary>
+        /// <param name="exception">The exception containing version compatibility information.</param>
+        /// <returns><see langword="true"/> if either the host engine version or declared constraint is missing; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static bool IsVersionIncompatible(this PluginIncompatibleException exception)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return string.IsNullOrEmpty(exception.HostEngineVersion) || string.IsNullOrEmpty(exception.DeclaredConstraint);
         }
 
+        /// <summary>
+        /// Gets a human-readable description of the incompatibility reason.
+        /// </summary>
+        /// <param name="exception">The exception containing version compatibility information.</param>
+        /// <returns>A formatted string describing the incompatibility.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static string GetIncompatibilityReason(this PluginIncompatibleException exception)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return $"Plugin {exception.DeclaredConstraint} is incompatible with host engine version {exception.HostEngineVersion}";
         }
 
+        /// <summary>
+        /// Determines whether the exception has a declared version constraint.
+        /// </summary>
+        /// <param name="exception">The exception containing version compatibility information.</param>
+        /// <returns><see langword="true"/> if a constraint is present; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static bool HasDeclaredConstraint(this PluginIncompatibleException exception)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return !string.IsNullOrEmpty(exception.DeclaredConstraint);
         }
     }
