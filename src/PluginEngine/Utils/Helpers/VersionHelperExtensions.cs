4874a35c7cd1e01d76d6ff72b38034515947991e
Vladyslav Zaiets <rutova2@gmail.com>
2024-06-18 11:32:47 +0300

    Handy extensions for VersionHelper
    mostly VersionHelperExtensions.cs, nothing exciting.

--- src/PluginEngine/Utils/Helpers/VersionHelperExtensions.cs
+++ src/PluginEngine/Utils/Helpers/VersionHelperExtensions.cs
@@ -2,25 +2,25 @@ using System;
 
 namespace PluginEngine.Utils.Helpers
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="VersionHelper"/> to simplify version comparison operations.
+    /// </summary>
     public static class VersionHelperExtensions
     {
         /// <summary>
         /// Determines whether the specified version string is greater than the target version.
         /// </summary>
-        /// <param name="helper">The version helper instance</param>
-        /// <param name="versionString">The version string to compare</param>
-        /// <param name="targetVersion">The target version to compare against</param>
-        /// <returns>True if versionString is greater than targetVersion</returns>
+        /// <param name="helper">The version helper instance.</param>
+        /// <param name="versionString">The version string to compare.</param>
+        /// <param name="targetVersion">The target version to compare against.</param>
+        /// <returns>True if versionString is greater than targetVersion; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="helper"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="versionString"/> or <paramref name="targetVersion"/> is null or whitespace.</exception>
         public static bool IsGreaterThan(this VersionHelper helper, string versionString, string targetVersion)
         {
-            if (helper == null)
-                throw new ArgumentNullException(nameof(helper));
-
-            if (string.IsNullOrWhiteSpace(versionString))
-                throw new ArgumentException("Version string cannot be null or whitespace", nameof(versionString));
-
-            if (string.IsNullOrWhiteSpace(targetVersion))
-                throw new ArgumentException("Target version cannot be null or whitespace", nameof(targetVersion));
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrEmpty(versionString);
+            ArgumentException.ThrowIfNullOrEmpty(targetVersion);
 
             return helper.CompareVersions(versionString, targetVersion) > 0;
         }
@@ -28,20 +28,17 @@ namespace PluginEngine.Utils.Helpers
         /// <summary>
         /// Determines whether the specified version string is less than the target version.
         /// </summary>
-        /// <param name="helper">The version helper instance</param>
-        /// <param name="versionString">The version string to compare</param>
-        /// <param name="targetVersion">The target version to compare against</param>
-        /// <returns>True if versionString is less than targetVersion</returns>
+        /// <param name="helper">The version helper instance.</param>
+        /// <param name="versionString">The version string to compare.</param>
+        /// <param name="targetVersion">The target version to compare against.</param>
+        /// <returns>True if versionString is less than targetVersion; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="helper"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="versionString"/> or <paramref name="targetVersion"/> is null or whitespace.</exception>
         public static bool IsLessThan(this VersionHelper helper, string versionString, string targetVersion)
         {
-            if (helper == null)
-                throw new ArgumentNullException(nameof(helper));
-
-            if (string.IsNullOrWhiteSpace(versionString))
-                throw new ArgumentException("Version string cannot be null or whitespace", nameof(versionString));
-
-            if (string.IsNullOrWhiteSpace(targetVersion))
-                throw new ArgumentException("Target version cannot be null or whitespace", nameof(targetVersion));
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrEmpty(versionString);
+            ArgumentException.ThrowIfNullOrEmpty(targetVersion);
 
             return helper.CompareVersions(versionString, targetVersion) < 0;
         }
@@ -49,20 +46,17 @@ namespace PluginEngine.Utils.Helpers
         /// <summary>
         /// Determines whether the specified version string is equal to the target version.
         /// </summary>
-        /// <param name="helper">The version helper instance</param>
-        /// <param name="versionString">The version string to compare</param>
-        /// <param name="targetVersion">The target version to compare against</param>
-        /// <returns>True if versionString is equal to targetVersion</returns>
+        /// <param name="helper">The version helper instance.</param>
+        /// <param name="versionString">The version string to compare.</param>
+        /// <param name="targetVersion">The target version to compare against.</param>
+        /// <returns>True if versionString is equal to targetVersion; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="helper"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="versionString"/> or <paramref name="targetVersion"/> is null or whitespace.</exception>
         public static bool IsEqualTo(this VersionHelper helper, string versionString, string targetVersion)
         {
-            if (helper == null)
-                throw new ArgumentNullException(nameof(helper));
-
-            if (string.IsNullOrWhiteSpace(versionString))
-                throw new ArgumentException("Version string cannot be null or whitespace", nameof(versionString));
-
-            if (string.IsNullOrWhiteSpace(targetVersion))
-                throw new ArgumentException("Target version cannot be null or whitespace", nameof(targetVersion));
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrEmpty(versionString);
+            ArgumentException.ThrowIfNullOrEmpty(targetVersion);
 
             return helper.CompareVersions(versionString, targetVersion) == 0;
         }
@@ -70,16 +64,15 @@ namespace PluginEngine.Utils.Helpers
         /// <summary>
         /// Gets the version information for a version string.
         /// </summary>
-        /// <param name="helper">The version helper instance</param>
-        /// <param name="versionString">The version string to parse</param>
-        /// <returns>ParsedVersionInfo containing major, minor, patch, and stability information</returns>
+        /// <param name="helper">The version helper instance.</param>
+        /// <param name="versionString">The version string to parse.</param>
+        /// <returns>ParsedVersionInfo containing major, minor, patch, and stability information.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="helper"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="versionString"/> is null or whitespace.</exception>
         public static ParsedVersionInfo GetVersionInfo(this VersionHelper helper, string versionString)
         {
-            if (helper == null)
-                throw new ArgumentNullException(nameof(helper));
-
-            if (string.IsNullOrWhiteSpace(versionString))
-                throw new ArgumentException("Version string cannot be null or whitespace", nameof(versionString));
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrEmpty(versionString);
 
             return helper.GetVersionInfo(versionString);
         }
@@ -87,16 +80,15 @@ namespace PluginEngine.Utils.Helpers
         /// <summary>
         /// Determines whether the specified version string is a valid semantic version.
         /// </summary>
-        /// <param name="helper">The version helper instance</param>
-        /// <param name="versionString">The version string to validate</param>
-        /// <returns>True if the version string is valid</returns>
+        /// <param name="helper">The version helper instance.</param>
+        /// <param name="versionString">The version string to validate.</param>
+        /// <returns>True if the version string is valid; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="helper"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="versionString"/> is null or whitespace.</exception>
         public static bool IsValidSemanticVersion(this VersionHelper helper, string versionString)
         {
-            if (helper == null)
-                throw new ArgumentNullException(nameof(helper));
-
-            if (string.IsNullOrWhiteSpace(versionString))
-                return false;
+            ArgumentNullException.ThrowIfNull(helper);
+            ArgumentException.ThrowIfNullOrEmpty(versionString);
 
             return helper.IsValidSemanticVersion(versionString);
         }
