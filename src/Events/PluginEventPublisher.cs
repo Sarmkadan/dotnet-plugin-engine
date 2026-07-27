e5cb3a0cb05010912ef186ad3289d4cdc7fd1963
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 12:50:08 +0300

    guard against re-entrant event publishing in PluginEventPublisher

---
+++ src/Events/PluginEventPublisher.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class PluginEventPublisher
+    {
+        private readonly object _lock = new object();
+        private readonly HashSet<PluginEventSubscriber> _subscribers = new HashSet<PluginEventSubscriber>();
+
+        public void Subscribe(PluginEventSubscriber subscriber)
+        {
+            lock (_lock)
+            {
+                _subscribers.Add(subscriber);
+            }
+        }
+
+        public void Unsubscribe(PluginEventSubscriber subscriber)
+        {
+            lock (_lock)
+            {
+                _subscribers.Remove(subscriber);
+            }
+        }
+
+        public async Task PublishAsync(PluginEvent pluginEvent, [CallerMemberName] string memberName = null)
+        {
+            await Task.Run(() =>
+            {
+                lock (_lock)
+                {
+                    foreach (var subscriber in _subscribers.ToList())
+                    {
+                        try
+                        {
+                            subscriber.HandleEvent(pluginEvent);
+                        }
+                        catch (Exception ex)
+                        {
+                            // Handle exception
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
