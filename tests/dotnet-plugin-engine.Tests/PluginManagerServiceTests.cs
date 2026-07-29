0c30847a8aa5bbfec5b390c30a0ace78316a52e6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 21:30:00 +0300

    update PluginManagerServiceTests

---
+++ tests/dotnet-plugin-engine.Tests/PluginManagerServiceTests.cs
@@ -0,0 +1,66 @@
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PluginEngine.Services.Implementations;
+using PluginEngine.Models;
+using Moq;
+
+namespace dotnet_plugin_engine.Tests
+{
+    public class PluginManagerServiceTests
+    {
+        [Fact]
+        public async Task TestInitializeAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestShutdownAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetStatusAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetAllPluginsAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetPluginsByStatusAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestActivatePluginAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestDeactivatePluginAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetPluginDetailsAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestSearchPluginsAsync()
+        {
+            // Test implementation
+        }
+    }
+}
