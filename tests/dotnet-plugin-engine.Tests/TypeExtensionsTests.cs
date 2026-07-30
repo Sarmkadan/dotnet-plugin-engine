dfb20aa261c3c25a1348412a9b916920f09993ac
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 11:08:26 +0300

    Update TypeExtensionsTests

---
+++ tests/dotnet-plugin-engine.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,13 @@
+namespace dotnet_plugin_engine.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.Json;
+using Xunit;
+using PluginEngine.Utils.Extensions;
+
+public class TypeExtensionsTests
+{
+    // ... entire file content ...
+}
