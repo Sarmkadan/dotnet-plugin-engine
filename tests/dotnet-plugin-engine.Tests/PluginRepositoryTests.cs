d0beb750ee8e97465c15a434d39f77f67b63fdce
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 06:22:49 +0300

    chore: tweak PluginRepositoryTests

---
+++ tests/dotnet-plugin-engine.Tests/PluginRepositoryTests.cs
@@ -0,0 +1,71 @@
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PluginEngine.Data.Repositories;
+using PluginEngine.Data.Models;
+
+namespace dotnet_plugin_engine.Tests
+{
+    public class PluginRepositoryTests
+    {
+        [Fact]
+        public async Task TestAddAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestUpdateAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestDeleteAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByIdAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByNameAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetAllAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByStatusAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestExistsAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestCountAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestSearchAsync()
+        {
+            // Test implementation
+        }
+    }
+}
