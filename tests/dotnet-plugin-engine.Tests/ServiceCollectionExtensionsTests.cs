dc06d173d2d105bb179062a25715141db71d16d1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 12:47:25 +0300

    update ServiceCollectionExtensionsTests

---
+++ tests/dotnet-plugin-engine.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,42 @@
+using Xunit;
+using PluginEngine.Utils.Extensions;
+using PluginEngine.Utils;
+using System;
+using System.Collections.Generic;
+using Moq;
+
+namespace dotnet_plugin_engine.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddPluginEngineStack_ValidInput_ReturnsServiceCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddPluginMiddleware_ValidInput_ReturnsServiceCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ConfigurePluginPipeline_ValidInput_ReturnsServiceCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddPluginEventHandler_ValidInput_ReturnsServiceCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void UsePluginRepository_ValidInput_ReturnsServiceCollection()
+        {
+            // Test implementation here
+        }
+    }
+}
