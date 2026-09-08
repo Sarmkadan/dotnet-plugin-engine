d708ac43188c10c86441d6f58c82d91c77ca12c6
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-08 16:26:23 +0000

    chore: apply codex edits

--- src/PluginEngine/GlobalUsings.cs
+++ src/PluginEngine/GlobalUsings.cs
@@ -9,12 +9,14 @@ global using System.Globalization;
 global using System.Reflection;
 global using System.Text;
 global using System.Xml;
+
 global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
+
 global using PluginEngine.Caching;
 global using PluginEngine.Configuration;
 global using PluginEngine.Data.Repositories;
